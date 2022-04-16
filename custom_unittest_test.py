da6ef847b785a608d22b31de2afd38c4934c740b
avishek-mondal <mondal.avishekri@gmail.com>
2022-04-16 10:41:32 +0100

    use lists instead of tuples

--- custom_unittest_test.py
+++ custom_unittest_test.py
@@ -2,6 +2,10 @@ from custom_unittest import CustomUnittest
 import unittest
 
 
+def one_arg(a):
+    return f"{str(a)}"
+
+
 def two_args(a, b):
     return f"{str(a)}{str(b)}"
 
@@ -18,19 +22,25 @@ def another_three_args(a, b, c):
     return f"{str(a)}{str(b)}{str(c)}"
 
 
+class OneArgsTest(CustomUnittest):
+    test_fns = [one_arg]
+    custom_test_cases = [
+        ([1], "1"),
+        ([3], "3"),
+    ]
 class TwoArgsTest(CustomUnittest):
     test_fns = [two_args, another_two_args]
     custom_test_cases = [
-        ((1, 2), "12"),
-        ((3, 4), "34"),
+        ([1, 2], "12"),
+        ([3, 4], "34"),
     ]
 
 
 class ThreeArgsTest(CustomUnittest):
     test_fns = [three_args, another_three_args]
     custom_test_cases = [
-        ((1, 2, 3), "123"),
-        ((3, 4, 5), "345"),
+        ([1, 2, 3], "123"),
+        ([3, 4, 5], "345"),
     ]
 
 
